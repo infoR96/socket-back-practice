d949d3a725904c4bdd4e2315a500adddd1d814a6
gian <giangc196@gmail.com>
2022-11-05 13:07:33 -0500

    backend de socket base

---
+++ models/sockets.js
@@ -0,0 +1,25 @@
+const BandList = require("./band-list");
+
+
+class Sockets {
+
+    constructor( io ) {
+
+        this.io = io;
+        this.bandList =new BandList(); 
+        this.socketEvents();
+    }
+
+    socketEvents() {
+        // On connection
+        this.io.on('connection', ( socket ) => {
+            console.log('Cliente concectado');
+            socket.emit('current-bands',this.bandList.getBands());
+        });
+    }
+
+
+}
+
+
+module.exports = Sockets;
